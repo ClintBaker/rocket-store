b27be36fda5d0710caa18e9d28f7047e1a208e53
Clint Baker <clintonjamesbaker@gmail.com>
2017-03-22 14:04:41 -0600

    shopping cart, and coffeescript functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'carts/show'
+
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :items
+  resource :cart, only: [:show] do
+    put 'add/:item_id', to: 'carts#add', as: :add_to
+    put 'remove/:item_id', to: 'carts#remove', as: :remove_from
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'items#index'
